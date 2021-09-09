4c4db218ffaeaabf99f4e1446b653237b1fa6ad2
guilherme <guilherme.godoi@allcomsistemas.com.br>
2021-09-09 17:04:27 -0300

    :sparkles: Adicionado prismic

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,16 @@
-import { NextPage } from 'next';
+import { GetServerSideProps, GetStaticProps, NextPage } from 'next';
 import { useCallback, useState } from 'react';
 import ReactPageScroller from 'react-page-scroller';
-import { Contato } from '../components/Template/Contato';
 
-import { Habilidades } from '../components/Template/Habilidades';
-import { Inicio } from '../components/Template/Inicio';
-import { Projetos } from '../components/Template/Projetos';
+import Prismic from '@prismicio/client';
+import { RichText } from 'prismic-dom';
+import { getPrismicClient } from '../services/prismic';
+
 import { Sobre } from '../components/Template/Sobre';
+import { Inicio } from '../components/Template/Inicio';
+import { Contato } from '../components/Template/Contato';
+import { Projetos } from '../components/Template/Projetos';
+import { Habilidades } from '../components/Template/Habilidades';
 
 const Home: NextPage = () => {
   const [currentPage, setCurrentPage] = useState(null);
@@ -41,6 +45,27 @@ const Home: NextPage = () => {
       {/* </ReactPageScroller> */}
     </>
   );
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+
+  const response = await prismic.query(
+    [Prismic.predicates.at('document.type', 'CardProjeto')],
+    {
+      fetch: ['cardprojeto.title', 'cardprojeto.description'],
+      pageSize: 20,
+    },
+  );
+
+  const card = response.results.map(post => ({
+    title: RichText.asText(post.data.title),
+  }));
+
+  console.log(response);
+  return {
+    props: {},
+  };
 };
 
 export default Home;
