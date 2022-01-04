c7731082abff83a880f2926157fe92e4afd352e7
Gui <guiggff@gmail.com>
2022-01-04 09:50:52 -0300

    :wrench: ajustes

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -37,6 +37,19 @@ export default class MyDocument extends Document {
     return (
       <Html lang="pt-BR">
         <Head>
+          <meta charSet="UTF-8" />
+          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
+
+          <meta
+            name="description"
+            content="Meu Portfólio pessoal de projetos."
+          />
+
           <link rel="shortcut icon" href="favicon.svg" type="image/x-icon" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
